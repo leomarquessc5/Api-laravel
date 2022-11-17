8b9a2e05209094e7b78aabc9b187bd689b2683a7
leonardo <leomarquessc5@gmail.com>
2022-11-17 14:27:21 -0300

    create students table

--- database/migrations/2022_11_17_164753_create_students_table.php
+++ database/migrations/2022_11_17_164753_create_students_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('students', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('name');
+            $table->string('course');
             $table->timestamps();
         });
     }
