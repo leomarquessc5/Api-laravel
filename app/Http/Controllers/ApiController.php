087e37eae02f4bb86998e5e9c930c411f8671cf8
leonardo <leomarquessc5@gmail.com>
2022-11-17 16:23:58 -0300

    fix methods

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,26 +3,65 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
 
 class ApiController extends Controller
 {
     public function getAllStudents() {
-      // logic to get all students goes here
+        $students = Student::get()->toJson(JSON_PRETTY_PRINT);
+        return response($students, 200);   
     }
 
     public function createStudent(Request $request) {
-      // logic to create a student record goes here
+        $student = new Student;
+        $student->name = $request->name;
+        $student->course = $request->course;
+        $student->save();
+    
+        return response()->json([
+            "message" => "Estudante criado com sucesso!"
+        ], 201);
     }
 
     public function getStudent($id) {
-      // logic to get a student record goes here
+        if (Student::where('id', $id)->exists()) {
+            $student = Student::where('id', $id)->get()->toJson(JSON_PRETTY_PRINT);
+            return response($student, 200);
+        }else {
+            return response()->json([
+              "message" => "Estudante não encontrado."
+            ], 404);
+        }
     }
 
-    public function updateStudent(Request $request, $id) {
-      // logic to update a student record goes here
+    public function updateStudent($id, Request $request) {
+        if (Student::where('id', $id)->exists()) {
+            $student = Student::find($id);
+            $student->name = is_null($request->name) ? $student->name : $request->name;
+            $student->course = is_null($request->course) ? $student->course : $request->course;
+            $student->update();
+            return response()->json([
+                "message" => "Estudante atualizado com sucesso!"
+            ], 200);
+        }else {
+            return response()->json([
+                "message" => "Estudante não encontrado."
+            ], 404);
+        }
     }
 
     public function deleteStudent ($id) {
-      // logic to delete a student record goes here
+        if(Student::where('id', $id)->exists()) {
+            $student = Student::find($id);
+            $student->delete();
+    
+            return response()->json([
+              "message" => "Estudante removido."
+            ], 202);
+        }else {
+            return response()->json([
+              "message" => "Estudante não encontrado."
+            ], 404);
+        }
     }
 }
