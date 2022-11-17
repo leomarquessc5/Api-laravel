65059f48b55f957935623376f08804ef17e84b0e
leonardo <leomarquessc5@gmail.com>
2022-11-17 14:27:36 -0300

    create api routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('students', 'ApiController@getAllStudents');
+Route::get('students/{id}', 'ApiController@getStudent');
+Route::post('students', 'ApiController@createStudent');
+Route::put('students/{id}', 'ApiController@updateStudent');
+Route::delete('students/{id}','ApiController@deleteStudent');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
